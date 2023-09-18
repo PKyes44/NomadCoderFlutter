7ae5c9040906bb2193165460660557fb589b7be8
PKyes44 <rugate23@gmail.com>
2023-09-18 22:04:25 +0900

    20230918-01

--- nettoon/lib/screens/home_screen.dart
+++ nettoon/lib/screens/home_screen.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:nettoon/models/webtoon_model.dart';
+import 'package:nettoon/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         title: const Text(
-          "오늘의 웹툰",
+          "Today's toon",
           style: TextStyle(
             fontSize: 24,
-            fontWeight: FontWeight.w400,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        builder: (context, futureResult) {
+          if (futureResult.hasData) {
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: futureResult.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = futureResult.data![index];
+
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        future: webtoons,
       ),
     );
   }
--- nettoon/lib/services/api_service.dart
+++ nettoon/lib/services/api_service.dart
@@ -1,17 +1,25 @@
 // ignore: depend_on_referenced_packages
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:nettoon/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      print(response.statusCode);
+      final webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
+      }
+      return webtoonInstances;
     }
+    throw Error();
   }
 }
